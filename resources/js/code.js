5e41c6c55343e2096f3303a211de410cb73c3e59
jtvd78 <jtvd78@aol.com>
2016-11-30 20:39:32 -0500

    changed book image Improved the sliding feature much more so it works on mobile Had to alter css js and index extensively to do it though It looks nice tho

--- resources/js/code.js
+++ resources/js/code.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
 
 var open = false;
 
+var setContent = function(id){
+
+	$(".mainContent").toggleClass("currentContent", false);
+	$(id).toggleClass("currentContent", true);
+
+	$(".mainContent").hide();
+	$(id).css('display','inline-block');
+};
+
 var start = function(){
 
 	//$("#menuImageContainer").click(function(event){
@@ -13,83 +22,79 @@ var start = function(){
 		//to add the overflow:visible when animating in order to keep the UI from messing up
 
 		if(!open){
+			$("#contentSlider").stop();
 			$(".leftMenuWidth").stop();
 			$(".leftMenuWidth").animate({width:"300px"},{duration:300,specialEasing:{width:'swing'}}).css('overflow','visible');
+			$("#contentSlider").animate({width:"250px"},{duration:300,specialEasing:{width:'swing'}});
 		}else{
 			$(".leftMenuWidth").stop();
+			$("#contentSlider").stop();
 			$(".leftMenuWidth").animate({width:"50px"},{duration:300,specialEasing:{width:'swing'}}).css('overflow','visible');
+			$("#contentSlider").animate({width:"0px"},{duration:300,specialEasing:{width:'swing'}});
 		}
+
 		open = !open;
 		
 	});
 
 	//Show the homepage
-	$(".mainContent").hide();
-	$("#homePageContent").show();
+	setContent("#homePageContent");
 
 	//Add listeners for left menu
 	$("#homePageMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#homePageContent").show();
+		setContent("#homePageContent");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#homePageMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
 	$("#educationMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#educationContent").show();
+		setContent("#educationContent");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#educationMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
+	//This and the one under it are almost the same
 	$("#nextbeatMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#nextbeatContainer").show();
+		setContent("#nextbeatContainer");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#nextbeatMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
 	$(".skipNB").click(function(){
-		$(".mainContent").hide();
-		$("#nextbeatContainer").show();
+		setContent("#nextbeatContainer");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#nextbeatMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
-
-
-
+	//Same as this one
 	$("#projectsMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#projectsContent").show();
+		setContent("#projectsContent");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#projectsMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
 	$(".skipProj").click(function(){
-		$(".mainContent").hide();
-		$("#projectsContent").show();
+		setContent("#projectsContent");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#projectsMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
+
 	$("#achievementsMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#achievementsContainer").show();
+		setContent("#achievementsContainer");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#achievementsMenuItem").toggleClass("currentContentMenuItem", true);
 	});
 
 	$("#scoutsMenuItem").click(function(){
-		$(".mainContent").hide();
-		$("#scoutsContainer").show();
+		setContent("#scoutsContainer");
 
 		$(".menuItemContainer").toggleClass("currentContentMenuItem", false);
 		$("#scoutsMenuItem").toggleClass("currentContentMenuItem", true);
@@ -100,4 +105,4 @@ var start = function(){
 		$("#" + childId).toggleClass("open closed");
 		console.log(childId);
 	});
-}
+};
